03bcc6564dbffe4c6cf968a10dfe1a00a8ebe5a8
Mustafa Hamdan <mhamdan@blulogix.com>
2022-12-30 17:56:54 +0300

    build componentes

--- Angular_e-commerce/src/app/app.module.ts
+++ Angular_e-commerce/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignUpComponent,
+    SignInComponent,
+    ProductsComponent,
+    OrdersComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
